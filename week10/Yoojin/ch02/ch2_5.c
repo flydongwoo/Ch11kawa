559ee1b3ea7069277da2c947133aeeca255dc249
hnyoojin <0dbwls21@gmail.com>
2024-10-02 21:43:31 +0900

    2장 문제 다풀었어요

---
+++ week10/Yoojin/ch02/ch2_5.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) 
+{
+	printf("7+8=%d\n",7+8);
+	printf("7-8=%d\n", 7-8);
+	printf("7*8=%d\n", 7*8);
+	printf("7/8=%f\n", 7/8);
+
+	return 0;
+}
