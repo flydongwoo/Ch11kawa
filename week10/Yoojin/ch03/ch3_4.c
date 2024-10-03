fce8bc69c78f19a316e92994e30deca8e1bc3f88
Hwang Yoojin <145079933+hnyoojin@users.noreply.github.com>
2024-10-03 17:52:24 +0900

    Update ch3_4.c

--- week10/Yoojin/ch03/ch3_4.c
+++ week10/Yoojin/ch03/ch3_4.c
@@ -3,11 +3,14 @@
 
 int main(void) 
 {
-	double F = 0;
+	double F = 0, C = 0;
 	printf("화씨값을 입력하시오: ");
 	scanf("%lf", &F);
 
-	printf("섭씨값은 %.2lf도 입니다.\n", 5 / 9 * (F - 32));
+    // 화씨 -> 섭씨 온도 계산식
+    C = 5.0 / 9.0 * (F - 32);
+
+	printf("섭씨값은 %.2lf도 입니다.\n", C);
 	
 	return 0;
 }
